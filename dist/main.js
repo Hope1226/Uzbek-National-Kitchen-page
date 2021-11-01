f6e273f077a4bf942029ad3ad72c4ba03ab31588
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-01 17:02:23 +0500

    Add nav and home page

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@
   \*****************************************************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"body {\\n  background: #7777bb;\\n}\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/style.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);\n// Imports\n\n\n\nvar ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./assets/images/hope-gb.jpg */ \"./src/assets/images/hope-gb.jpg\"), __webpack_require__.b);\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n___CSS_LOADER_EXPORT___.push([module.id, \"@import url(https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap);\"]);\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"html {\\n  -webkit-box-sizing: border-box;\\n  -moz-box-sizing: border-box;\\n  box-sizing: border-box;\\n  font-family: \\\"Poppins\\\", sans-serif;\\n}\\n\\n*,\\n*::before,\\n*::after {\\n  -webkit-box-sizing: inherit;\\n  -moz-box-sizing: inherit;\\n  box-sizing: inherit;\\n  padding: 0;\\n  margin: 0;\\n}\\n\\nbody {\\n  background: #43434f url(\" + ___CSS_LOADER_URL_REPLACEMENT_0___ + \") no-repeat;\\n  background-size: cover;\\n  color: #fff;\\n}\\n\\n.layer {\\n  background: linear-gradient(rgba(0, 0, 0, 0.2) 10%, rgba(0, 0, 0, 0.487) 40%, #000 100%);\\n  position: absolute;\\n  top: 0;\\n  bottom: 0;\\n  left: 0;\\n  right: 0;\\n}\\n\\n.container {\\n  width: 90%;\\n  max-width: 900px;\\n  margin: 0 auto;\\n  padding: 20px 0;\\n}\\n.container .main-nav {\\n  display: grid;\\n  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));\\n  grid-template-rows: 1fr;\\n  grid-gap: 0;\\n  justify-items: stretch;\\n  align-items: stretch;\\n}\\n.container .main-nav a {\\n  padding: 10px 20px;\\n  border-right: 1px solid #fff;\\n  text-align: center;\\n}\\n.container .main-nav a:last-child {\\n  border-right: none;\\n}\\n.container .main-nav a:hover {\\n  cursor: pointer;\\n  color: wheat;\\n}\\n.container .homeCont {\\n  display: grid;\\n  grid-template-columns: 1fr;\\n  grid-template-rows: 1fr 1fr;\\n  grid-gap: 20px;\\n  justify-items: stretch;\\n  align-items: stretch;\\n  padding: 50px 0;\\n}\\n.container .homeCont .headline {\\n  padding: 100px 0;\\n  display: flex;\\n  flex-direction: column;\\n  justify-content: center;\\n  align-items: center;\\n}\\n.container .homeCont .headline .hero-text {\\n  text-align: center;\\n  background: rgba(0, 0, 0, 0.474);\\n  mix-blend-mode: overlay;\\n  font-size: 50px;\\n}\\n.container .homeCont .headline small {\\n  font-size: 20px;\\n  align-self: flex-end;\\n}\\n.container .homeCont .reservationForm {\\n  width: 60%;\\n  max-width: 100%;\\n  display: grid;\\n  grid-template-columns: 1fr;\\n  grid-template-rows: auto auto auto 1fr;\\n  grid-gap: 10px;\\n  justify-items: stretch;\\n  align-items: stretch;\\n  justify-self: center;\\n  font-size: 500;\\n}\\n.container .homeCont .reservationForm > * {\\n  padding: 10px;\\n  background: rgba(0, 0, 0, 0.672);\\n  color: #fff;\\n  border: none;\\n  backdrop-filter: blur(8px);\\n  border-radius: 5px;\\n  color: #414141;\\n  font-size: 16px;\\n  color: #ff6f00;\\n}\\n.container .homeCont .reservationForm label {\\n  display: flex;\\n  flex-direction: row;\\n  justify-content: space-between;\\n  align-items: stretch;\\n}\\n.container .homeCont .reservationForm label input {\\n  background: transparent;\\n  border: none;\\n  color: #ff6f00;\\n}\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/style.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -27,6 +27,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((module) => {
 
 eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n  Author Tobias Koppers @sokra\n*/\nmodule.exports = function (cssWithMappingToString) {\n  var list = []; // return the list of modules as css string\n\n  list.toString = function toString() {\n    return this.map(function (item) {\n      var content = \"\";\n      var needLayer = typeof item[5] !== \"undefined\";\n\n      if (item[4]) {\n        content += \"@supports (\".concat(item[4], \") {\");\n      }\n\n      if (item[2]) {\n        content += \"@media \".concat(item[2], \" {\");\n      }\n\n      if (needLayer) {\n        content += \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\");\n      }\n\n      content += cssWithMappingToString(item);\n\n      if (needLayer) {\n        content += \"}\";\n      }\n\n      if (item[2]) {\n        content += \"}\";\n      }\n\n      if (item[4]) {\n        content += \"}\";\n      }\n\n      return content;\n    }).join(\"\");\n  }; // import a list of modules into the list\n\n\n  list.i = function i(modules, media, dedupe, supports, layer) {\n    if (typeof modules === \"string\") {\n      modules = [[null, modules, undefined]];\n    }\n\n    var alreadyImportedModules = {};\n\n    if (dedupe) {\n      for (var k = 0; k < this.length; k++) {\n        var id = this[k][0];\n\n        if (id != null) {\n          alreadyImportedModules[id] = true;\n        }\n      }\n    }\n\n    for (var _k = 0; _k < modules.length; _k++) {\n      var item = [].concat(modules[_k]);\n\n      if (dedupe && alreadyImportedModules[item[0]]) {\n        continue;\n      }\n\n      if (typeof layer !== \"undefined\") {\n        if (typeof item[5] === \"undefined\") {\n          item[5] = layer;\n        } else {\n          item[1] = \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\").concat(item[1], \"}\");\n          item[5] = layer;\n        }\n      }\n\n      if (media) {\n        if (!item[2]) {\n          item[2] = media;\n        } else {\n          item[1] = \"@media \".concat(item[2], \" {\").concat(item[1], \"}\");\n          item[2] = media;\n        }\n      }\n\n      if (supports) {\n        if (!item[4]) {\n          item[4] = \"\".concat(supports);\n        } else {\n          item[1] = \"@supports (\".concat(item[4], \") {\").concat(item[1], \"}\");\n          item[4] = supports;\n        }\n      }\n\n      list.push(item);\n    }\n  };\n\n  return list;\n};\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./node_modules/css-loader/dist/runtime/api.js?");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+/*!********************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
+  \********************************************************/
+/***/ ((module) => {
+
+eval("\n\nmodule.exports = function (url, options) {\n  if (!options) {\n    options = {};\n  }\n\n  if (!url) {\n    return url;\n  }\n\n  url = String(url.__esModule ? url.default : url); // If url is already wrapped in quotes, remove them\n\n  if (/^['\"].*['\"]$/.test(url)) {\n    url = url.slice(1, -1);\n  }\n\n  if (options.hash) {\n    url += options.hash;\n  } // Should url be wrapped?\n  // See https://drafts.csswg.org/css-values-3/#urls\n\n\n  if (/[\"'() \\t\\n]|(%20)/.test(url) || options.needQuotes) {\n    return \"\\\"\".concat(url.replace(/\"/g, '\\\\\"').replace(/\\n/g, \"\\\\n\"), \"\\\"\");\n  }\n\n  return url;\n};\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./node_modules/css-loader/dist/runtime/getUrl.js?");
 
 /***/ }),
 
@@ -110,13 +120,43 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 
 /***/ }),
 
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav */ \"./src/nav.js\");\n\n\nconst createHome = (container) => {\n  let homeCont = document.createElement('div');\n  homeCont.classList.add('homeCont');\n  let headline = document.createElement('div');\n  headline.classList.add('headline')\n  let heroTxt = document.createElement('h1');\n  heroTxt.innerHTML = \"Wellcome to Uzbek National kitchen\"\n  heroTxt.classList.add('hero-text');\n  let subHero = document.createElement('small');\n  subHero.innerHTML = \"Reserve a place now!\";\n\n  headline.appendChild(heroTxt);\n  headline.appendChild(subHero);\n\n  let form = document.createElement('form');\n  form.classList.add('reservationForm');\n  let inputName = document.createElement('input');\n  inputName.type = \"text\";\n  inputName.placeholder = \"Enter your name\";\n  let inputEmail = document.createElement('input');\n  inputEmail.type = \"emial\";\n  inputEmail.placeholder = \"Enter your email\"\n  let labelFor = document.createElement('label');\n  labelFor.innerHTML = \"Please enter the date of the reservation\"\n  let inputDate = document.createElement('input');\n  inputDate.type = \"date\";\n  inputDate.id = \"date\";\n  let textArea = document.createElement('textarea');\n  textArea.innerHTML = 'Enter your message here';\n  let submitButton = document.createElement('button');\n  submitButton.type = 'submit';\n  submitButton.id = \"submitBtn\";\n  submitButton.innerHTML = \"Place the reservation\";\n  labelFor.appendChild(inputDate);\n\n  form.appendChild(inputName);\n  form.appendChild(inputEmail);\n  form.appendChild(labelFor);\n  form.appendChild(textArea);\n\n  homeCont.appendChild(headline);\n  homeCont.appendChild(form);\n\n  container.appendChild(homeCont);\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createHome);\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/home.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n\n\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./nav */ \"./src/nav.js\");\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n\n\n\n\nconst mainContainer = document.querySelector('.container');\n\nmainContainer.appendChild((0,_nav__WEBPACK_IMPORTED_MODULE_1__[\"default\"])());\n(0,_home__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(mainContainer);\n\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/nav.js":
+/*!********************!*\
+  !*** ./src/nav.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst createNav = () => {\n  let navigation = document.createElement('nav');\n  navigation.classList.add('main-nav');\n  let homeLink = document.createElement('a');\n  homeLink.innerHTML = 'Home';\n  homeLink.id = \"homeLink\" \n  let menuLink = document.createElement('a'); \n  menuLink.innerHTML = 'Menu';\n  menuLink.id = \"menuLink\"\n  let contactLink = document.createElement('a');\n  contactLink.innerHTML = 'Contacts';\n  contactLink.id = \"contactLink\"\n  \n  navigation.appendChild(homeLink);\n  navigation.appendChild(menuLink);\n  navigation.appendChild(contactLink);\n\n  return navigation\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createNav);\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/nav.js?");
+
+/***/ }),
+
+/***/ "./src/assets/images/hope-gb.jpg":
+/*!***************************************!*\
+  !*** ./src/assets/images/hope-gb.jpg ***!
+  \***************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"76f021bfe7de8ebfb90c.jpg\";\n\n//# sourceURL=webpack://uzbek-national-kitchen-page/./src/assets/images/hope-gb.jpg?");
 
 /***/ })
 
@@ -146,6 +186,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
@@ -171,6 +214,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -185,6 +240,52 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
 /******/ 	})();
 /******/ 	
 /************************************************************************/
---
+++ src/home.js
@@ -0,0 +1,49 @@
+import createNav from "./nav";
+
+const createHome = (container) => {
+  let homeCont = document.createElement('div');
+  homeCont.classList.add('homeCont');
+  let headline = document.createElement('div');
+  headline.classList.add('headline')
+  let heroTxt = document.createElement('h1');
+  heroTxt.innerHTML = "Wellcome to Uzbek National kitchen"
+  heroTxt.classList.add('hero-text');
+  let subHero = document.createElement('small');
+  subHero.innerHTML = "Reserve a place now!";
+
+  headline.appendChild(heroTxt);
+  headline.appendChild(subHero);
+
+  let form = document.createElement('form');
+  form.classList.add('reservationForm');
+  let inputName = document.createElement('input');
+  inputName.type = "text";
+  inputName.placeholder = "Enter your name";
+  let inputEmail = document.createElement('input');
+  inputEmail.type = "emial";
+  inputEmail.placeholder = "Enter your email"
+  let labelFor = document.createElement('label');
+  labelFor.innerHTML = "Please enter the date of the reservation"
+  let inputDate = document.createElement('input');
+  inputDate.type = "date";
+  inputDate.id = "date";
+  let textArea = document.createElement('textarea');
+  textArea.innerHTML = 'Enter your message here';
+  let submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.id = "submitBtn";
+  submitButton.innerHTML = "Place the reservation";
+  labelFor.appendChild(inputDate);
+
+  form.appendChild(inputName);
+  form.appendChild(inputEmail);
+  form.appendChild(labelFor);
+  form.appendChild(textArea);
+
+  homeCont.appendChild(headline);
+  homeCont.appendChild(form);
+
+  container.appendChild(homeCont);
+};
+
+export default createHome;
